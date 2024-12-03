6d3c47fbce6d921a03ba587c1558b3cf3fde93fd
yukimelty <yuki.it.engineer@gmail.com>
2024-12-04 00:31:57 +0900

    change stackname & add removalPolicy

--- cdk/lib/s3stack.ts
+++ cdk/lib/s3stack.ts
@@ -3,13 +3,15 @@ import { Construct } from "constructs";
 import * as s3 from "aws-cdk-lib/aws-s3";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class CdkStack extends cdk.Stack {
+export class S3Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     const bucket = new s3.Bucket(this, "CreateBucket", {
       bucketName: "test-bucket-20241125-ap",
       versioned: false,
+      // スタック削除時にs3バケットも削除
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
     });
   }
 }
